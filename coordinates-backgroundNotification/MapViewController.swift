f75f2eda8eccc20d7225c2222c5948c464c3a93e
Kent Huang <kenthuang426@gmail.com>
2017-10-04 18:15:47 -0700

    add gitignore

---
+++ coordinates-backgroundNotification/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coordinates-backgroundNotification
+//
+//  Created by Yu Hong Huang on 2017-10-04.
+//  Copyright © 2017 Yu Hong Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ coordinates-backgroundNotification/Region.swift
@@ -0,0 +1,9 @@
+//
+//  Region.swift
+//  coordinates-backgroundNotification
+//
+//  Created by Yu Hong Huang on 2017-10-04.
+//  Copyright © 2017 Yu Hong Huang. All rights reserved.
+//
+
+import Foundation
